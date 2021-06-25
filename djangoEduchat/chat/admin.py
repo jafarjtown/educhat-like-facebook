ae491efbd454ff5754a565e29398c0ac3a2893ba
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2021-06-25 16:01:43 +0100

    Delete djangoEduchat directory

--- djangoEduchat/chat/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Chat, Messages
-# Register your models here.
-
-admin.site.register(Chat)
-admin.site.register(Messages)
--- djangoEduchat/user_profile/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FollowUnFollow
-from django.views.decorators.csrf import csrf_exempt
-urlpatterns = [
-		path('follow-onfollow',csrf_exempt(FollowUnFollow))
-]
